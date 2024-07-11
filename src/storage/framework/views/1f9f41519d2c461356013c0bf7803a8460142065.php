90be799407ecfd9c47f9ce96d37237420ce833b8
keke <kekeisozaki13@gmail.com>
2024-07-11 20:42:43 +0900

    更新

---
+++ src/storage/framework/views/1f9f41519d2c461356013c0bf7803a8460142065.php
@@ -0,0 +1,46 @@
+<?php $__env->startSection('css'); ?>
+<link rel="stylesheet" href="<?php echo e(asset('css/confirm.css')); ?>" />
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="confirm__content">
+      <div class="confirm__heading">
+        <h2>お問い合わせ内容確認</h2>
+      </div>
+      <form class="form" action="/contacts" method="post">
+        <?php echo csrf_field(); ?>
+        <div class="confirm-table">
+          <table class="confirm-table__inner">
+            <tr class="confirm-table__row">
+              <th class="confirm-table__header">お名前</th>
+              <td class="confirm-table__text">
+                <input type="text" name="name" value="<?php echo e($contact['name']); ?>" readonly />
+              </td>
+            </tr>
+            <tr class="confirm-table__row">
+              <th class="confirm-table__header">メールアドレス</th>
+              <td class="confirm-table__text">
+                <input type="email" name="email" value="<?php echo e($contact['email']); ?>" readonly />
+              </td>
+            </tr>
+            <tr class="confirm-table__row">
+              <th class="confirm-table__header">電話番号</th>
+              <td class="confirm-table__text">
+                <input type="tel" name="tel" value="<?php echo e($contact['tel']); ?>" readonly />
+              </td>
+            </tr>
+            <tr class="confirm-table__row">
+              <th class="confirm-table__header">お問い合わせ内容</th>
+              <td class="confirm-table__text">
+                <input type="text" name="content" value="<?php echo e($contact['content']); ?>" readonly />
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">送信</button>
+        </div>
+      </form>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/resources/views/confirm.blade.php ENDPATH**/ ?>
